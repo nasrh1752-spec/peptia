9af0e4f1645a548f249a811c64983b0e4279615d
nasrh1752-spec <nasrh1752@gmail.com>
2026-09-02 20:41:52 +0300

    Update shared.js

--- shared.js
+++ shared.js
@@ -8,7 +8,7 @@ window.Peptia = (function () {
   'use strict';
 
   // Constants
-  const PRODUCTS_DATA_VERSION = "2";
+  const PRODUCTS_DATA_VERSION = "3";
   const STORE_KEY  = 'peptia_products_v' + PRODUCTS_DATA_VERSION;
   const WA_KEY     = 'whatsappNumber';
   const WA_DEFAULT = '201095673317';
